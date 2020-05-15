d4fd295beab5b6b02ebcccf4e029d795bde04c44
31373773 <55849726+31373773@users.noreply.github.com>
2020-05-15 11:20:27 -0400

    Task: Updates

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ mysql.init_app(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Steph'}
+    user = {'username': 'Zillow Project'}
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM TableZillow')
     result = cursor.fetchall()
@@ -98,21 +98,42 @@ def api_retrieve(house_id) -> str:
     return resp
 
 
-@app.route('/api/v1/houses/', methods=['POST'])
-def api_add() -> str:
-    resp = Response(status=201, mimetype='application/json')
-    return resp
-
-
 @app.route('/api/v1/houses/<int:house_id>', methods=['PUT'])
 def api_edit(house_id) -> str:
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    inputData = (content['Indx'], content['Living_Space_sq_ft'], content['Beds'],
+                 content['Baths'], content['Zip'],
+                 content['Year'], content['List_Price'],house_id)
+    sql_update_query = """UPDATE TableZillow t SET t.Indx = %s, t.Living_Space_sq_ft = %s, t.Beds = %s, t.Baths = 
+        %s, t.Zip = %s, t.Year = %s, t.List_Price = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
+@app.route('/api/v1/houses', methods=['POST'])
+def api_add() -> str:
+
+    content = request.json
+
+    cursor = mysql.get_db().cursor()
+    inputData = (content['Indx'], content['Living_Space_sq_ft'], content['Beds'],
+                 content['Baths'], content['Zip'],
+                 content['Year'], request.form.get('List_Price'))
+    sql_insert_query = """INSERT INTO TableZillow (Indx, Living_Space_sq_ft,Beds,Baths,Zip,Year,List_Price) VALUES (%s, %s,%s, %s,%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
     resp = Response(status=201, mimetype='application/json')
     return resp
 
-
-@app.route('/api/houses/<int:house_id>', methods=['DELETE'])
+@app.route('/api/v1/houses/<int:house_id>', methods=['DELETE'])
 def api_delete(house_id) -> str:
-    resp = Response(status=210, mimetype='application/json')
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM TableZillow WHERE id = %s """
+    cursor.execute(sql_delete_query, house_id)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
     return resp
 
 
